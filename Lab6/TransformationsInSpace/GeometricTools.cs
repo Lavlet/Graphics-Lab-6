3c682c4a25bd027b66e87a8d75b23fca09a0a003
Andrey <andrey_pozdnyakov@epam.com>
2017-12-13 00:48:13 +0400

    Добавляю пропущенные файлы

---
+++ Lab6/TransformationsInSpace/GeometricTools.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TransformationsInSpace
+{
+    public static class GeometricTools
+    {
+        public static double PlaneDotDistance(Point point, Plane plain)
+        {
+            return Math.Abs(plain.A * point.X + plain.B * point.Y + plain.C * point.Z + plain.D) /
+                Math.Sqrt(Math.Pow(plain.A, 2) + Math.Pow(plain.B, 2) + Math.Pow(plain.C, 2));
+        }
+
+        public static double IsVisible(Point camera, Plane plane)
+        {
+            return DotProduct(camera, plane);
+        }
+
+        public static double Sqr(double a)
+        {
+            return a * a;
+        }
+
+        public static double DotProduct(double[,] a, double[,] b)
+        {
+            return a[0, 0] * b[0, 0] + a[0, 1] * b[0, 1] + a[0, 2] * b[0, 2];
+        }
+
+        public static double DotProduct(Point camera, Plane plane)
+        {
+            return plane.A * camera.X + plane.B * camera.Y + plane.C * camera.Z;
+        }
+
+        public static double[][] ToJagged(double[,] source)
+        {
+            var result = new double[source.GetLength(0)][];
+
+            for (int i = 0; i < source.GetLength(0); i++)
+            {
+                result[i] = new double[source.GetLength(1)];
+                for (int j = 0; j < source.GetLength(1); j++)
+                {
+                    result[i][j] = source[i, j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lab6/TransformationsInSpace/Plane.cs
@@ -0,0 +1,37 @@
+﻿namespace TransformationsInSpace
+{
+    public class Plane
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+        public double D { get; private set; }
+
+        public Plane(Point a, Point b, Point c)
+        {
+            var temp1 = (b.Y - a.Y) * (c.Z - a.Z) - (c.Y - a.Y) * (b.Z - a.Z);
+            var temp2 = (b.X - a.X) * (c.Z - a.Z) - (c.X - a.X) * (b.Z - a.Z);
+            var temp3 = (b.X -a.X) * (c.Y-a.Y) - (c.X - a.X) * (b.Y - a.Y)  ;
+
+            A = temp1;
+            B = -temp2;
+            C = temp3;
+            D = -a.X * temp1 + a.Y * temp2 - a.Z * temp3;
+
+            if (D == 0 && A == 0 && B == 0 && C != 0)
+                C = 1;
+            else if (D == 0 && A == 0 && B != 0 && C == 0)
+                B = 1;
+            else if (D == 0 && A != 0 && B == 0 && C == 0)
+                A = 1;
+        }
+
+        public Plane(Point a, double[] normal)
+        {
+            A = normal[0];
+            B = normal[1];
+            C = normal[2];
+            D = normal[0] * (-a.X) + normal[1] * (-a.Y) + normal[2] * (-a.Z);
+        }
+    }
+}
